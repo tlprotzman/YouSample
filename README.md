ae84534f8c5dad94a514646c6ed54af94118899e
Tristan Protzman <tlprotzman+github@gmail.com>
2018-12-19 00:26:31 -0500

    Improvements made to sequencer

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 A YouTube Sampler Machine
 
 [Try it out!](https://tlprotzman.github.io/YouSample/)
+
+## To Do
+* Flesh out sample manipulation
+* Apply manipulations to sequenced data
+* Add more samples or measures
+* Video playback 
+    * Might be too much of a stretch
+* Better documentation/help
+* Show currently playing note
--- src/js/sequencer.js
+++ src/js/sequencer.js
@@ -2,8 +2,17 @@ let state = new Array(0);
 let table = null;
 let beats = 0;
 let beat_len = 0;
+let num_bars = 0;
 let num_divs = 0;
 let marker_div = 0;
+let measure_div =0;
+let first_initialization = true;
+
+let playing = true;
+let start_time = 0;
+let note_time = 0;
+let current_step = 0;
+let dt = 0.2;
 
 const play_button = document.getElementById("play_sequence");
 const stop_button = document.getElementById("stop_sequence");
@@ -11,38 +20,64 @@ const stop_button = document.getElementById("stop_sequence");
 play_button.addEventListener('click', play);
 stop_button.addEventListener('click', stop);
 
-
-function toggleBeat(cell, i, j) {
-    // console.log("I: " + i.toString());
-    // console.log("J: " + j.toString());
-    if (state[i][j]) {
-        cell.className = "";
-        if (!(j % marker_div)) {
-            cell.className = "beat";
-        }
+class Tile  {
+    constructor() {
+        this.active = false;
+        this.bar_line = false;
+        this.current = false;
+        this.cell = null;
     }
-    else {
-        cell.className = "clicked"
-    }
-    state[i][j] = !state[i][j];
 }
 
-function makegrid() {
+function updateTile(i, j) {
+    let tile = state[i][j];
+    console.log(tile.current);
+    if (tile.active) {
+        tile.cell.className = "clicked";
+    } else if (tile.current) {
+        tile.cell.className = "current";
+    } else if (tile.bar_line) {
+        tile.cell.className = "beat";
+    }
+    else {
+        tile.cell.className = "";
+    }
+}
+
+function toggleBeat(cell, i, j) {
+    state[i][j].active = !state[i][j].active;
+    updateTile(i, j);
+}
+
+function make_grid() {
+    if (!first_initialization) {
+        if (!confirm("This will clear any pattern present! Are you sure?")) {
+            return;
+        }
+    }
+    first_initialization = false;
     table = document.getElementById("sequence_in");
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
     beats = parseInt(document.getElementById("num_beats").value);
     beat_len = parseInt(document.getElementById("beat_length").value);
+    num_bars = parseInt(document.getElementById("num_bars").value);
     num_divs = 0;
     marker_div = 0;
     switch (beat_len) {
-        case 2: num_divs = beats * 4; marker_div = 4; break;
-        case 4: num_divs = beats * 2; marker_div = 2; break;
-        case 8: num_divs = beats * 3; marker_div = 3; break;
+        case 2: num_divs = num_bars * beats * 2; marker_div = 4; measure_div = 8; break;
+        case 4: num_divs = num_bars * beats * 4; marker_div = 4; measure_div = 16; break;
+        case 8: num_divs = num_bars * beats; marker_div = beats; measure_div = 6; break;
         default: alert("Unknown beat length: " + beat_len.toString());
     }
 
-    state = new Array(num_samples);
-    for (let i = 0;  i < num_samples; i++) {
-        state[i] = new Array(num_divs);
+    state = new Array(num_samples + 1);
+    for (let i = 0;  i < num_samples + 1; i++) {
+        state[i] = new Array(num_divs + 1);
+        for (let j = 0; j < num_divs + 1; j++) {
+            state[i][j] = new Tile();
+        }
     }
 
     let beat_count = 1;
@@ -50,12 +85,16 @@ function makegrid() {
         let row = table.appendChild(document.createElement("tr"));
         for (let j = 0; j < num_divs + 1; j++) {
             let cell = row.appendChild(document.createElement("td"));
+            state[i][j].cell = cell;
             // Place the sample name in the first column
             if (i == 0) {
                 if (j && (j-1) % marker_div == 0) {
-                    cell.innerHTML = beat_count.toString();
-                    beat_count++;
                     cell.className = "beat";
+                    state[i][j].bar_line = true;
+                    if ((j-1) % measure_div == 0) {
+                        cell.innerHTML = beat_count.toString();
+                        beat_count++;
+                    }
                 }
                 continue
             }
@@ -66,24 +105,43 @@ function makegrid() {
             else {
                 cell.addEventListener('click', function(cell, i, j){
                     return function(){toggleBeat(cell, i, j)}
-                }(cell, i - 1, j - 1));
+                }(cell, i, j));
                 cell.innerHTML = "";
             }
             // Denote beats
             if (j && (j - 1) % marker_div == 0) {
                 cell.className = "beat";
+                state[i][j].bar_line = true;
             }
 
         }
     }
+    update_bpm();
 }
 
+function markCurrentNote(note) {
+    note++;
+    let last_note = note - 1;
+    if (last_note == 0) {
+        last_note = num_divs;
+    }
+    for (let i = 0; i < num_samples + 1; i++) {
+        state[i][note].current = true;
+        updateTile(i, note);
+        state[i][last_note].current = false;
+        updateTile(i, last_note);
+    }
+}
 
-let playing = true;
-let start_time = 0;
-let note_time = 0;
-let current_step = 0;
-let dt = 0.2;
+function update_bpm() {
+    let bpm = parseInt(document.getElementById("bpm").value);
+    switch (beat_len) {
+        case 2: dt = (60 / bpm) / 2; break;
+        case 4: dt = (60 / bpm) / 4; break;
+        case 6: dt = (60 / bpm) / 3; break;
+        default: dt = (60 / bpm) / 4; break;
+    }
+}
 
 
 function scheduleNote() {
@@ -111,13 +169,14 @@ function nextNote() {
 
 function playPatternStepAtTime(play_time) {
     for (let i = 0; i < num_samples; i++) {
-        if (state[i][current_step]) {
+        if (state[i+1][current_step+1].active) {
             let source = context.createBufferSource();
             source.buffer = samples[i];
             source.connect(context.destination);
             source.start(play_time);
         }
     }
+    markCurrentNote(current_step);
 }
 
 function play() {
